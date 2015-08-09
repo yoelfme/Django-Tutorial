2587c3d73c936040afa155d7b75ee1c5c07918a5
Yoel Monzon <yoelfme@hotmail.com>
2015-08-08 22:37:33 -0600

    [add] index view

--- polls/admin.py
+++ polls/admin.py
@@ -11,7 +11,7 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date Information', {'fields':  ['pub_date'], 'classes': ['collapse']})
+        ('Date Information', {'fields':  ['pub_date']})
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
