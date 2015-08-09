d01fbfa284b652f62b6664aba558e526d0e1d470
Yoel Monzon <yoelfme@hotmail.com>
2015-08-09 11:03:25 -0600

    [update] function to view detail of question

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from .models import Question
 
@@ -15,7 +15,14 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s" % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exists")
+
+    return render(request, 'polls/detail.html', {
+        'question': question
+    })
 
 
 def results(request, question_id):
